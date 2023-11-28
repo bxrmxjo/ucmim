f87a216724d9bd35cabb73c2ff594cda05ba9a5f
bxrmxjo <76953086+bxrmxjo@users.noreply.github.com>
2023-11-28 09:45:59 +0100

    Update jacobi.m

--- jacobi.m
+++ jacobi.m
@@ -1,23 +1,28 @@
 A = input("Introduce la matriz para resolver el sistema Au = b: ");
 b = input("Introduce el segundo miembro (b) del sistema entre []: ");
+b = b(:); 
 m = input("Introduce cuál será el número máximo de iteraciones: ");
 e = input("Introduce la precisión de parada deseada: ");
 
 n = size(A,1);
-
-r = (sqrt((e^2)/n))*ones(n,1); %para que su módulo sea el mismo que el del error deseado, y se ejecute el bucle while
 d = zeros(n,1);
 u = zeros(n,1);
+r = b - A*u;
+nb = norm(b);
 
 k = 0;
-while k <= m && norm(r) >= e 
+while k <= m && norm(r) >= e*nb 
     k = k + 1;
-    for i=1:n
-        r(i) = b(i) - (A(i,1:n)*u(1:n));
-        d(i) = r(i)/A(i,i);
-        u(i) = u(i) + d(i);
-    end
+
+    r = b - A*u;
+    d = r./diag(A); %El comando "./" se usa para dividir componente a componente.
+    u = u + d;
+
 end
 
-disp(['En ' num2str(k-1) ' iteraciones se ha obtenido el siguiente resultado :']);
+%Al salir del bucle, ya sea por alcanzar las iteraciones máximas o por
+%obtener el resultado con la tolerancia deseada, se muestran las
+%iteraciones realizadas.
+
+disp(['En ' num2str(k) ' iteraciones se ha obtenido el siguiente resultado :']);
 disp(u)
